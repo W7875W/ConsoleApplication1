c8b13abd32a1c6e710ff6cb5bb5268db7b670195
Tuchka <48439439+W7875W@users.noreply.github.com>
2022-03-23 15:53:11 +0200

    Update ConsoleApplication1.cpp

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <iomanip>
 #include <vector>
 #include <cmath>
@@ -7,13 +7,7 @@ using namespace std;
 
 int main()
 {
-	/*
-	string s, s2 = "q";
-	cin >> s;
-	s2 = s2 + s;
-	cout << s2;
-	*/
-	
+	//word palindrome
 	string s;
 	int i = 0,n=0;
 	getline(cin, s);
@@ -44,159 +38,5 @@ int main()
 		cout << "yes";
 	}
 	else { cout << "no"; }
+	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-	/*
-	string s;
-	char x='!';
-	char y = '))';
-	getline(cin, s);
-	for (auto c : s) {
-		cout << c;
-		if (c == x) {
-			cout << y;
-		}
-	}
-	*/
-	/*
-	int num = 1;
-	string s;
-	getline(cin, s);
-	for (auto c : s) {
-		cout << c;
-		if (c == ' ') {
-			num++;
-			cout << endl;
-			cout << num << ".";
-		}
-	}
-	*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-int n, m;
-cin >> n >> m;
-int a[100][100];
-for (int i = 0; i < n; i++) {
-	for (int j = 0; j < m; j++) {
-		cin >> a[i][j];
-	}
-}
-int k, l;
-cin >> k >> l;
-for (int i = 0; i < n; i++) {
-	for (int j = 0; j < m; j++) {
-		if (i >= k && j == l) {
-			cout << a[i][j] << " ";
-			cout << a[i][j - l] << " ";
-		}
-		else if (i>=k && j > l) {
-			cout << a[i][j] << " ";
-		}
-		else if (i<k){ cout << a[i][j] << " "; }
-	}
-	cout << endl;
-}
-*/
-/*
-if (i == j + 1) {
-	a[i][j] = 1;
-}
-else if (i == j + 2) {
-	a[i][j] = 2;
-}
-else if (i == j + 3) {
-	a[i][j] = 3;
-}
-else if (i == j + 4) {
-	a[i][j] = 4;
-}
-else if (i == j - 1) {
-	a[i][j] = 1;
-}
-else if (i == j - 2) {
-	a[i][j] = 2;
-}
-else if (i == j - 3) {
-	a[i][j] = 3;
-}
-else if (i == j - 4) {
-	a[i][j] = 4;
-}*/
-
-/*if (z < a[i][j]) {
-	z = a[i][j];
-	if (x == 0 && y == 0) {
-		x = i;
-		y = j;
-	}
-	else if (i < x) {
-		x = i;
-		y = j;
-	}
-	else if (j < y) {
-		x = i;
-		y = j;
-	}
-}*/
-/*
-int x,y,z,i=0;
-cin >> x;
-y = x;
-z = 0;
-//1 2 2 1
-//
-//0 y z x
-while (x!=0) {
-	//if (z <= x) { z = 0; }
-	if (x > y) {
-		z = x;
-	}
-	else if (z>x && z>y) {
-		i++;
-		y = x;
-		z = 0;
-	}
-	y = x;
-	cin >>x;
-}
-cout << i;
-//x>y=z
-//y>x&&y>z
-//z>x i++//x>y z=x;*/
