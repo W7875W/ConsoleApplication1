94cf9326fd75ca5b8c01655231e77d21a9de10ce
Tuchka <48439439+W7875W@users.noreply.github.com>
2022-06-16 16:01:04 +0300

    Add files via upload

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,42 +1,67 @@
-#include <iostream>
+﻿#include <iostream>
 #include <iomanip>
 #include <vector>
 #include <cmath>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string>
+#include <set>
+#include <algorithm>
+
 using namespace std;
+
 
 int main()
 {
-	//word palindrome
-	string s;
-	int i = 0,n=0;
-	getline(cin, s);
-	for (auto c : s) {
-		i++;
-		//cout << c;
-	}
-	/*
-	for (int j = i-1; j >= 0; j--) {
-		//cout << s[j];
-		for (auto c : s) {
-			//cout << c;
-			if (s[j] == c) {
-				n++;
-			}
+	int a, b, c, d,max,min,main;
+	cin >> a >> b >> c;
+	//	   5    2    3 input
+	//     2    3    5 output
+	max = a;
+	min = a;
+	if (max < b && max < c) {
+		if (b > c) {
+			max = b;
+		}
+		else if (b < c) {
+			max = c;
+		}
+		else if(b==c) {
+			max = c;
 		}
 	}
-	*/
-	for (int j = i - 1,k=0; j >= 0; j--,k++) {
-		//cout << s[j];
-			//cout << c;
-		if (s[j] == s[k]) {
-			n++;
+	else if (max<b) {
+		max = b;
+	}
+	else if (max < c) {
+		max = c;
+	}
+	if (min > b && min > c) {
+		if (b < c) {
+			min = b;
 		}
-
+		else if (b > c) {
+			min = c;
+		}
+		else if (b == c) { 
+			min = b; 
+		}
 	}
-	if (i == n) {
-		cout << "yes";
+	else if (min > b) {
+		min = b;
 	}
-	else { cout << "no"; }
+	else if (min > c) {
+		min = c;
+	}
+	if ((c >= b && c <= a)||(c<=b&&c>=a)) {
+		main = c;
+	}
+	else if ((b >= c && b <= a) || (b<=c && b>=a)) {
+		main = b;
+	}
+	else if ((a >= b && a <= c) || (a<=b && a>=c)) {
+		main = a;
+	}
+	cout << min<<" "<<main<< " " << max;
 	return 0;
 }
